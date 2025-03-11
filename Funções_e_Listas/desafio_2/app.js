c140933d2bd6998fea6a97348eb1b20675ca23e8
Andrielly <andry_psa@hotmail.com>
2025-03-10 22:32:36 -0300

    inclusão desafio 2

---
+++ Funções_e_Listas/desafio_2/app.js
@@ -0,0 +1,29 @@
+// Função que exibe "Olá, mundo!" no console
+function exibirMensagem() {
+    console.log("Olá, mundo!");
+}
+
+// Função que recebe um nome e exibe "Olá, [nome]!" no console
+function cumprimentar(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+
+// Função que recebe um número e retorna o dobro
+function dobrarNumero(numero) {
+    return numero * 2;
+}
+
+// Função que recebe três números e retorna a média
+function calcularMedia(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+
+// Função que recebe dois números e retorna o maior
+function maiorNumero(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+// Função que recebe um número e retorna o resultado da multiplicação desse número por ele mesmo
+function quadradoNumero(numero) {
+    return numero * numero;
+}
